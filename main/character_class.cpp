f5eb5cf082ace7dddcde3c4f42814c0772d402fd
Grégoire <gregoirelefevre26@gmail.com>
2026-03-22 22:33:05 +0100

    small tests

---
+++ main/character_class.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+class Character {
+protected:
+  std::string name, type;
+  int hp, maxHp, level;
+
+public:
+  Character(std::string pName, std::string pType, int pMaxHp, int pLevel) {
+    name = pName;
+    type = pType;
+    maxHp = pMaxHp;
+    hp = maxHp;
+    level = pLevel;
+  }
+
+  // Getters
+  std::string getName() { return name; }
+
+  std::string getType() { return type; }
+
+  int getHp() { return hp; }
+
+  int getMaxHp() { return maxHp; }
+
+  int getLevel() { return level; }
+
+  // Setters
+  void setHp(int pHp) { hp = pHp; }
+
+  void setMaxHp(int pMaxHp) { maxHp = pMaxHp; }
+
+  void setLevel(int pLevel) { level = pLevel; }
+
+  // Methods
+  virtual void attack() {
+    std::cout << name << " uses a basic " << type << " attack!" << std::endl;
+  }
+
+  void takeDamage(int damage) {
+    hp -= damage;
+    if (hp < 0)
+      hp = 0;
+    std::cout << name << " took " << damage << " damage! (HP: " << hp << "/"
+              << maxHp << ")\n";
+  }
+};
--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,11 @@
+#include "character_class.cpp"
 #include <iostream>
 
-int main () {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main() {
+  Character char1("Greg", "Human", 100, 1);
+  char1.attack();
+  char1.takeDamage(10);
+  std::cout << char1.getName() << " has " << char1.getHp() << " HP."
+            << std::endl;
+  return 0;
 }
